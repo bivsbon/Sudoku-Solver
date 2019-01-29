2a087d2955bcb9cc82093ab454a4038530c47a77
Minh Trinh <bivsbon69@outlook.com>
2019-01-29 21:01:34 +0700

    Add solving timer

--- SudokuSolver/Main/Main.cpp
+++ SudokuSolver/Main/Main.cpp
@@ -1,16 +1,25 @@
 #include "pch.h"
 #include <iostream>
 #include "Board.h"
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 int main()
 {
 	Board brd;
 	brd.getInput();
+
+	// Solve and count solving time
+	auto start = steady_clock::now();
 	brd.solve();
+	auto end = steady_clock::now();
+	const duration<float> time = end - start;
+	float solvingTime = time.count();
+
 	cout << endl;
-	cout << "Solved!";
+	cout << "Solved! " << "(" << solvingTime << " seconds )" << endl;
 	
 	getchar();
 	return 0;
